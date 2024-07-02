6533553a050d792634197243670ea6d8aa82b79f
kodyberry23 <kodyberry23@gmail.com>
2024-07-02 14:13:37 -0400

    initial commit

---
+++ lib/ex_finix.ex
@@ -0,0 +1,2 @@
+defmodule ExFinix do
+end
---
+++ test/ex_finix_test.exs
@@ -0,0 +1,8 @@
+defmodule ExFinixTest do
+  use ExUnit.Case
+  doctest ExFinix
+
+  test "greets the world" do
+    assert ExFinix.hello() == :world
+  end
+end
