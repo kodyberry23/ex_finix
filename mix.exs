a26a5de40e410aa6acba585152329829774e4555
Kody Berry <kodyberry@Kodys-MacBook-Pro.local>
2024-10-05 13:23:47 -0400

    bump hex version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Finix.MixProject do
   def project do
     [
       app: :ex_finix,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       description: description(),
